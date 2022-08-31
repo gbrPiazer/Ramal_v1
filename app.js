cebf78fcab4ba5584ee8e57c145cbed07b78f504
gbrPiazer <gabrielmpiazer@gmail.com>
2022-08-31 15:15:23 -0300

    modified:   app.js 	modified:   public/script/main.js 	modified:   views/index.handlebars 	modified:   views/layouts/main.handlebars 	modified:   views/partials/_lista_ramal.handlebars 	deleted:    views/partials/_lista_sala.handlebars 	modified:   views/partials/_lista_setores.handlebars

--- app.js
+++ app.js
@@ -51,7 +51,7 @@
 
 //Rotas
         app.get('/', async (req, res) => {            
-            Ramal.find({}).sort({setor: 'asc', nome_func: 'asc'}).populate("setor").then((ramais) => {
+            Ramal.find({}).sort({setor: 'desc', nome_func: 'asc'}).populate("setor").then((ramais) => {
                 Setores.find().then((setores) => {
                     res.render("index", {ramais: ramais, setores: setores})
                     })
@@ -60,12 +60,6 @@
                 req.flash("error_msg", "Houve um erro ao carregar o formulário")
                 res.render("index")
             })
-        })
-
-        app.post("/extrairFunc", async (req, res) => {
-            let payload = req.body.payload.trim();
-            let search = await Ramal.find({nome_func: {$regex: new RegExp('^'+payload+'.*', 'i')}}).populate("setor").exec();
-            res.send({payload: search});
         })
         app.use('/admin', admin)
 //Outros
--- public/script/main.js
+++ public/script/main.js
@@ -1,35 +1,12 @@
-const searchResults = document.getElementById('searchResults')
-
-function sendData(e){
-  let match = e.value.match(/^[a-zA-Z]*/)
-  let match2 = e.value.match(/\s*/)
-  if(match2[0] === e.value){
-    searchResults.innerHTML = '{{>_lista_ramal}}';
-  }
-  fetch("extrairFunc", {
-    method: "POST",
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({payload: e.value})
-  }).then(res => res.json()).then(data => {
-    let payload = data.payload;
-    console.log(payload)
-    searchResults.innerHTML = '';
-    if(payload.length < 1){
-      searchResults.innerHTML = '<tr> <td></td> <td> Nenhum funcionário encontrado </td><td></td><td></td> </tr>';
-    }
-    payload.forEach((item, index) => {
-      searchResults.innerHTML += `
-      <tr>
-        <td scope="row">${item.setor.nome}</td>
-        <td scope="row">${item.nome_func}</td>
-        <td scope="row"><a class="btn btn-outline-danger" href="tel:${item.ramal}">${item.ramal}</a></td>
-        <td scope="row"><a class="btn btn-outline-danger" href="mailto:${item.email}">${item.email}</a></td>
-      </tr>
-      `
+$(document).ready(function(){
+  $('#searchBar').on('keyup', function(){
+    var value = $(this).val().toLowerCase();
+    $("#searchResults tr").filter(function(){
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     })
   })
-}
+})
 
-function cliQue(){
-  searchResults.innerHTML = '{{>_lista_ramal}}';
-}
+$('#f1').on('click', function(){
+  $('#searchResults').html('>_lista_filas');
+})
